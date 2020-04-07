8137bb70975ad3d4b643c9768b4ffb6f54dcc342
Nagamani Maddi <nagamani.maddi@gmail.com>
2020-04-07 14:05:05 +0530

    first commit

---
+++ src/main/java/com/agile/openClosedPrinciple/App.java
@@ -0,0 +1,13 @@
+package com.agile.openClosedPrinciple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
